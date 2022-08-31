6ad590cf86f35e7b8d65f7bf78e0554d59c0787c
AlanLiu1470 <glxbwycw@gmail.com>
2022-08-31 13:44:48 -0400

    1

--- Labs/Lab2/sketch.js
+++ Labs/Lab2/sketch.js
@@ -7,13 +7,14 @@ function setup() {
 
 function draw() { 
   console.log("1");
-  fill(0);
+  fill(25);
   ellipse(50,50,90,90);
 
   strokeWeight(20);
+  fill(190)
   ellipse(350,350,200,200)
 
-  fill(255);
+  fill(153);
   strokeWeight(1);
   rect(100,100,100,200);
 }
