9f6bb744529a6f2dd90212b6b0ecb832a48dca2f
AlanLiu1470 <glxbwycw@gmail.com>
2022-09-08 19:13:23 -0400

    Exercise

---
+++ Labs/3 - World Wrap/sketch.js
@@ -0,0 +1,17 @@
+var radius = 50;
+var x = -radius;
+var speed = 5;
+
+function setup(){
+console.log("1");
+createCanvas(800,600);
+}
+
+function draw(){
+fill(0,0,255);
+ellipse(x,50,radius);
+    x += speed;
+    if(x > width + radius){
+        x = -radius;
+    }
+}
